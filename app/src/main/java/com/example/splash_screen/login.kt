50ba77dde55f3f4885b6f58d4f20fb20d0fdf428
ankita <ankitasinghckw@gmail.com>
2024-04-08 23:16:09 +0530

    login and register backend

--- app/src/main/java/com/example/splash_screen/login.kt
+++ app/src/main/java/com/example/splash_screen/login.kt
@@ -2,25 +2,70 @@ package com.example.splash_screen
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
 import com.example.splash_screen.databinding.ActivityLoginBinding
 import com.example.splash_screen.databinding.ActivitySignUpBinding
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import java.time.Instant
 
 class login : AppCompatActivity() {
     private val binding: ActivityLoginBinding by lazy {
         ActivityLoginBinding.inflate(layoutInflater)
     }
+
+    private lateinit var firebaseAuth: FirebaseAuth
+//    override fun onStart() {
+//        super.onStart()
+//        val currentUser : FirebaseUser ? = firebaseAuth.currentUser
+//
+//        if(currentUser !=null){
+//            startActivity(Inten
+    //            t(this,MainActivity::class.java))
+//        }
+//    }
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
         setContentView(binding.root)
-        binding.signUpbutton.setOnClickListener {
+
+        firebaseAuth= FirebaseAuth.getInstance()
+
+
+        binding.logInbutton.setOnClickListener {
+            //get text from sign up page
+
+            val username=binding.useremail.text.toString()
+            val password=binding.password.text.toString()
+            if( username.isEmpty() || password.isEmpty() ){
+                Toast.makeText(this, "PLEASE FILL ALL THE DETAILS", Toast.LENGTH_SHORT).show()
+            }
+            else{
+                firebaseAuth.signInWithEmailAndPassword(username,password)
+                    .addOnCompleteListener { task ->
+                        if(task.isSuccessful) {
+                            Toast.makeText(this, "SIGN-IN SUCCESSFUL", Toast.LENGTH_SHORT).show()
+                            startActivity(Intent(this,MainActivity::class.java))
+                            finish()
+                        }
+                        else{
+                            Toast.makeText(this, "SIGN-IN FAILED  ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                        }
+
+                        }
+
+
+                    }
+            }
+            binding.signUpbutton.setOnClickListener {
             startActivity(Intent(this,sign_up::class.java))
             finish()
         }
+
 
     }
 }
--- app/src/main/java/com/example/splash_screen/sign_up.kt
+++ app/src/main/java/com/example/splash_screen/sign_up.kt
@@ -2,6 +2,7 @@ package com.example.splash_screen
 
 import android.content.Intent
 import android.os.Bundle
+import android.widget.Toast
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -19,15 +20,34 @@ class sign_up : AppCompatActivity() {
         super.onCreate(savedInstanceState)
 
         setContentView(binding.root)
+
+        firebaseAuth= FirebaseAuth.getInstance()
         binding.signInbutton.setOnClickListener {
             startActivity(Intent(this,login::class.java))
             finish()
         }
 binding.registerbutton.setOnClickListener {
+    //get text from sign up page
     val email=binding.email.text.toString()
-    val username=binding.username.toString()
-    val password=binding.password.toString()
+    val username=binding.username.text.toString()
+    val password=binding.password.text.toString()
 
+    if(email.isEmpty() || username.isEmpty() || password.isEmpty() ){
+        Toast.makeText(this, "PLEASE FILL ALL THE DETAILS", Toast.LENGTH_SHORT).show()
+    }
+    else{
+        firebaseAuth.createUserWithEmailAndPassword(email,password)
+            .addOnCompleteListener (this){task ->
+                if(task.isSuccessful){
+                    Toast.makeText(this, "REGISTRATION SUCCESSFUL", Toast.LENGTH_SHORT).show()
+                    startActivity(Intent(this,login::class.java))
+                    finish()
+                }
+                else{
+                    Toast.makeText(this, "REGISTRATION FAILED  ${task.exception?.message}", Toast.LENGTH_SHORT).show()
+                }
+            }
+    }
 }
     }
 }
